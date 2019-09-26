6a4b67f32619e7c1f176f6fb7cdde9f0b6f80c04
baker ho <bakerho@tcd.ie>
2019-09-26 20:13:27 +0100

    Fix to handle all cases.

--- test.py
+++ test.py
@@ -7,22 +7,62 @@ class Node:
         current.left = None
         current.right = None
     # Function that returns pointer of LCA of two nodes
-def findLCA(root, node1, node2):
+    # found1 is set as true by this function if node1 is found
+    # found2 is set as true by this function if node2 is found
+def findLCAUtil(root, node1, node2, found):
 
     if root is None:    #Base Case
-        return None;
+        return None
 
-    if root.key == node1 or root.key == node2: # Node1/2 is LCA Case
+    if root.key == node1: # Node1 is LCA Case
+        found[0] = True # Found node1 = True
+        return root
+    if root.key == node2: # Node1 is LCA Case
+        found[1] = True # Found node2 = True
         return root
 
-    left_lca = findLCA (root.left, node1, node2) # Traverse Left and right subtrees, searching for node 1 and node 2
-    right_lca = findLCA (root.right, node1, node2)
+    left_lca = findLCAUtil(root.left, node1, node2, found) # Traverse Left and right subtrees, searching for node 1 and node 2
+    right_lca = findLCAUtil(root.right, node1, node2, found)
 
     if left_lca and right_lca: # if both functions return Non-Null, then current node is LCA
         return root
 
     return left_lca if left_lca is not None else right_lca # Otherwise check if left or right subtree is lCA
+
+def find(root, k):
+
+    # Base Case
+    if root is None:
+        return False
+
+    # If key is present at root, or if left subtree or right
+    # subtree , return true
+    if (root.key == k or find(root.left, k) or find(root.right, k)):
+        return True
+
+    # Else return false
+    return False
+
+# This function returns LCA of n1 and n2 onlue if both
+# n1 and n2 are present in tree, otherwise returns None
+def findLCA(root, node1, node2):
+
+    # Initialize n1 and n2 as not visited
+    found = [False, False]
+
+    # Find lac of n1 and n2 using the technique discussed above
+    lca = findLCAUtil(root, node1, node2, found)
+
+    # Returns LCA only if both n1 and n2 are present in tree
+    if (found[0] and found[1] or found[0] and find(lca, node2) or found[1] and
+        find(lca, node1)):
+        return lca
+
+    # Else return None
+    return None
 #Test Temporary Tree with nodes
+
+# Driver program to test above function
 root = Node(1)
 root.left = Node(2)
 root.right = Node(3)
@@ -30,7 +70,16 @@ root.left.left = Node(4)
 root.left.right = Node(5)
 root.right.left = Node(6)
 root.right.right = Node(7)
-print("LCA(4,5) = ",findLCA(root, 4, 5).key)
-print("LCA(4,6) = ",findLCA(root, 4, 6).key)
-print("LCA(3,4) = ",findLCA(root, 3, 4).key)
-print("LCA(2,4) = ",findLCA(root, 2, 4).key)
+
+lca = findLCA(root, 4, 5)
+
+if lca is not None:
+    print ("LCA(4, 5) =", lca.key)
+else :
+    print ("Keys are not present")
+
+lca = findLCA(root, 4, 10)
+if lca is not None:
+    print ("LCA(4,10) = ", lca.key)
+else:
+    print ("Keys are not present")
