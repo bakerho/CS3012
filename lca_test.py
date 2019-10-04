50bac9aa3138ae1e13e73793bf5c53f77f44b548
baker ho <bakerho@tcd.ie>
2019-10-04 10:56:55 +0100

    Add test for a node not present in tree

--- lca_test.py
+++ lca_test.py
@@ -21,4 +21,29 @@ class TestLca:
         lca1 = lca.findLCA(root, 4, 5)
         assert lca1 is None, "test_null failed"
 
-    
+    def test_nonpresent_node(self): # Test case for a node not present in tree
+        root = Node(1)
+        root.left = Node(2)
+        root.right = Node(3)
+        root.left.left = Node(4)
+        root.left.right = Node(5)
+        root.right.left = Node(6)
+        root.right.right = Node(7)
+
+        lca1 = lca.findLCA(root, 4, 8)
+        assert lca1 is None, "test_nonpresent_node failed"
+
+        node1 = lca.find(root,4) #Test each node indivdually
+        assert node1 is True, "test_nonpresent_node node1 failed"
+        node2 = lca.find(root,8)
+        assert node2 is False, "test_nonpresent_node node1 failed"
+
+    #def test_root_node(self):# Test case where root is a node of interest for lca
+
+    #def test_lowest_ndde (self): # Test case with one of the lowest nodes in tree
+
+    #def test_left_right_node (self): # Test case with one node in the furthest left tree and the other in the furthest right tree
+
+    #def test_two_nodes_with_equal_keys(self): # Test case with two nodes of equal keys
+
+    #def test_three_nodes_with_equal_keys(self): # Test case with three nodes of equal keys, but only need to find two
