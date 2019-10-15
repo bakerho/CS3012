71c5862f18d3b7914440f7bd0fe3f87d91502a48
baker ho <bakerho@tcd.ie>
2019-10-15 13:22:01 +0100

    Add test for lowest node in tree

--- dag_test.py
+++ dag_test.py
@@ -1,28 +1,28 @@
 from dag import daglca, Node
 
 class TestDag:
-    def test_basic(self):
-        node0 = Node(1)
+    def test_basic(self): #Test for basic case, nodes present in graph
+        root = Node(1)
         node1 = Node(2)
         node2= Node(3)
         node3 = Node (4)
         node4 = Node (5)
         node5 = Node (6)
-        node0.succ = [node1,node2,node3,node4]
+        root.succ = [node1,node2,node3,node4]
         node1.succ = [node3]
-        node1.pred = [node0]
+        node1.pred = [root]
         node2.succ = [node3,node4]
-        node2.pred = [node0]
+        node2.pred = [root]
         node3.succ = [node4]
-        node3.pred = [node1,node2,node0]
-        node4.pred = [node2,node3,node0]
+        node3.pred = [node1,node2,root]
+        node4.pred = [node2,node3,root]
         node5.pred = [node3]
 
-        lca = daglca (node0, node3,node4)
+        lca = daglca (root, node3,node4)
 
-        assert lca is 3
+        assert lca is 3, "test_basic failed"
 
-    def test_root_node(self):
+    def test_root_node(self): # Test case where root is a node of interest for lca
         root = Node(1)
         node1 = Node(2)
         node2 = Node(3)
@@ -38,11 +38,32 @@ class TestDag:
 
         lca = daglca(root, node1, root)
 
-        assert lca is 1
+        assert lca is 1, "test_root_node failed"
 
-    def test_null(self):
+    def test_null(self): # Test case with empty graph
         root = None
 
         lca = daglca(root,1,2)
 
-        assert lca is None
+        assert lca is None,  "test_null failed"
+
+    def test_lowest_node (self): # Test case with one of the lowest nodes in graph
+        root = Node(1)
+        node1 = Node(2)
+        node2= Node(3)
+        node3 = Node (4)
+        node4 = Node (5)
+        node5 = Node (6)
+        root.succ = [node1,node2,node3,node4]
+        node1.succ = [node3]
+        node1.pred = [root]
+        node2.succ = [node3,node4]
+        node2.pred = [root]
+        node3.succ = [node4]
+        node3.pred = [node1,node2,root]
+        node4.pred = [node2,node3,root]
+        node5.pred = [node3]
+
+        lca = daglca (root, node5,node1)
+
+        assert lca is 1, "test_lowest_node failed"
